9eafa98ffeffb2edcd51e3d6d060c3330f8992db
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2021-07-18 21:05:49 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,7 +37,6 @@ addLayer("i", {
     }
 })
 
-/*
 addLayer("g", {
     symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -50,7 +49,7 @@ addLayer("g", {
     resource: "types of googols", // Name of prestige currency
     baseResource: "illions", // Name of resource prestige is based on
     baseAmount() {return player.i.points}, // Get the current amount of baseResource
-    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 10, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
@@ -66,4 +65,4 @@ addLayer("g", {
     layerShown(){return true},
     upgrades: {}
 })
-*/
+
