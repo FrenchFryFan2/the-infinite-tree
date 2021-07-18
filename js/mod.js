f6f29ba6eb84d4a688af4e5d864276037903ae11
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2021-07-18 21:40:11 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,14 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Basic Illions",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Added illions and types of googols.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
