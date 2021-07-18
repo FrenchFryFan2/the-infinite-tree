5bc46e9e95ec299234313fb5c13e467d40fcb72e
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2021-07-18 20:05:37 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -39,11 +39,11 @@ function canGenPoints(){
 
 // Calculate points/sec!
 function getPointGen() {
-	if (hasUpgrade('i', 11)) gain = gain.times(1.005)
 	if(!canGenPoints())
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
+	if (hasUpgrade('i', 11)) gain = gain.exp(1.005)
 	return gain
 }
 
