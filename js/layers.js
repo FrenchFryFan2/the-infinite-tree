413bdf03c19fea09638b4b3facf2671f0acc1f99
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2021-07-18 20:48:38 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -37,3 +37,33 @@ addLayer("i", {
 	    }
     }
 })
+
+addLayer("g", {
+    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: player.illions.points >= 100,
+		points: new Decimal(0),
+    }},
+    color: "#1291b5",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "types of googols", // Name of prestige currency
+    baseResource: "illions", // Name of resource prestige is based on
+    baseAmount() {return player.illions.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 10, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "g", description: "G: Reset for types of googols", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {}
+})
