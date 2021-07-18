a1b9c2eaf10b74247645ce60d47c421c8141b3a0
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2021-07-18 21:09:12 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -40,8 +40,9 @@ addLayer("i", {
 addLayer("g", {
     symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    branches: ["i"],
     startData() { return {
-        unlocked: player.i.points >= 100,
+        unlocked: layers.i.points >= 100,
 		points: new Decimal(0),
     }},
     color: "#1291b5",
