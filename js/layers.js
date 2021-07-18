d8b21200390622b12a49108b49ca040b94abab80
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2021-07-18 20:54:16 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,4 @@
 addLayer("i", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "I", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -38,8 +37,8 @@ addLayer("i", {
     }
 })
 
+/*
 addLayer("g", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -60,10 +59,11 @@ addLayer("g", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "g", description: "G: Reset for types of googols", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
     upgrades: {}
 })
+*/
