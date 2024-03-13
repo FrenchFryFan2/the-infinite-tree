9d44f8894bcec4dd62053bc9c5341b2952043404
Dackel090 <105323132+Dackel090@users.noreply.github.com>
2024-03-13 17:03:56 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -9,7 +9,7 @@ addLayer("p", {
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "atoms", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "particles", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -24,5 +24,11 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
+    upgrades: {
+        11: {
+
+        },
+    },
+
     layerShown(){return true}
 })
