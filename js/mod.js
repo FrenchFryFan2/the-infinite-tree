ae906f30163d417d0b2d6a914929c54a13dcc102
Dackel090 <105323132+Dackel090@users.noreply.github.com>
2024-03-13 16:44:25 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The TechTree Tree",
-	id: "mymod",
+	name: "The Universal Tree",
+	id: "tttt",
 	author: "dackel09",
-	pointsName: "points",
+	pointsName: "particles",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Start of Nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
